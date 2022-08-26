32cd1eb340f6b142223798d09c2ec0136323052b
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-08-26 21:41:56 +0700

    add configs/database and godotenv dependency

---
+++ configs/database.go
@@ -0,0 +1,48 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func DatabaseConnection() *gorm.DB {
+
+	errEnv := godotenv.Load()
+
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dns := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, dbName)
+	db, errDB := gorm.Open(mysql.Open(dns), &gorm.Config{})
+
+	if errDB != nil {
+		panic("Failed to Connect Database")
+	}
+
+	// TODO: Add Model to Migrate
+	// db.AutoMigrate()
+
+	return db
+
+}
+
+func DatabaseRejection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+
+	if err != nil {
+		panic("Failed to Rejection Database")
+	}
+
+	dbSQL.Close()
+
+}
--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
-
 func main() {
-	dsn := "ro0t:P@5sw012D@tcp(127.0.0.1:3306)/go-rest-api?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		log.Fatal("Connection DB Error :", err.Error())
-	}
-
-	fmt.Println("DB Connected Successfully")
 
 }
