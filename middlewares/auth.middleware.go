483600ec290810523b1c7fe5b5d5850f4b759d52
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-08-27 14:31:58 +0700

    add auth middleware

---
+++ middlewares/auth.middleware.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"go-rest-api/helpers"
+	"go-rest-api/services"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func Authorize(jwtService services.JWTService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			response := helpers.BuildResponseError("Sorry, You're Unauthorized", "Token not found", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		token, err := jwtService.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id]: ", claims["user_id"])
+			log.Println("Claim[issuer]: ", claims["issuer"])
+		} else {
+			log.Println(err)
+			response := helpers.BuildResponseError("Sorry, Token Is Not Valid", err.Error(), nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+	}
+}
+
+// TODO: add user access permisition
+func HasAccess() {
+
+}
