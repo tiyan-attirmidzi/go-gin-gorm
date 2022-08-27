cf7bdfb8c226400345f62fcea586f8577ef7a6e3
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-08-27 19:15:58 +0700

    refactor response name

--- middlewares/auth.middleware.go
+++ middlewares/auth.middleware.go
@@ -15,7 +15,7 @@ func Authorize(jwtService services.JWTService) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		authHeader := ctx.GetHeader("Authorization")
 		if authHeader == "" {
-			response := helpers.BuildResponseError("Sorry, You're Unauthorized", "Token not found", nil)
+			response := helpers.ResponseError("Sorry, You're Unauthorized", "Token not found", nil)
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 			return
 		}
@@ -26,7 +26,7 @@ func Authorize(jwtService services.JWTService) gin.HandlerFunc {
 			log.Println("Claim[issuer]: ", claims["issuer"])
 		} else {
 			log.Println(err)
-			response := helpers.BuildResponseError("Sorry, Token Is Not Valid", err.Error(), nil)
+			response := helpers.ResponseError("Sorry, Token Is Not Valid", err.Error(), nil)
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 		}
 	}
