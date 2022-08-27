d1c55adbea5851e3c401964bc4d0b090d24cbd44
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-08-27 12:37:16 +0700

    add dto auth, book and user and add go-jwt

--- dto/book.go
+++ dto/book.go
@@ -1,13 +1,13 @@
 package dto
 
-type BookCreateDTO struct {
-	ID          uint64 `json:"id" form:"id" binding:"required"`
+type BookCreate struct {
 	Title       string `json:"title" form:"title" binding:"required"`
 	Description string `json:"description" form:"description" binding:"required"`
 	UserID      uint64 `json:"user_id,omitempty" form:"user_id,omitempty"`
 }
 
-type BookUpdateDTO struct {
+type DTOBookUpdateDTO struct {
+	ID          uint64 `json:"id" form:"id" binding:"required"`
 	Title       string `json:"title" form:"title" binding:"required"`
 	Description string `json:"description" form:"description" binding:"required"`
 	UserID      uint64 `json:"user_id,omitempty" form:"user_id,omitempty"`
