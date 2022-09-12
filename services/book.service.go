9262eb9ed484d6fcce65e220209fc7f6dd8c0f0d
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-09-12 11:19:47 +0700

    add book services

---
+++ services/book.service.go
@@ -0,0 +1,64 @@
+package services
+
+import (
+	"log"
+
+	"github.com/mashingan/smapping"
+	"github.com/tiyan-attirmidzi/go-rest-api/dto"
+	"github.com/tiyan-attirmidzi/go-rest-api/entities"
+	"github.com/tiyan-attirmidzi/go-rest-api/repositories"
+)
+
+type BookService interface {
+	Index() []entities.Book
+	Show(bookID uint64) entities.Book
+	Store(book dto.BookCreate) entities.Book
+	Update(book dto.BookUpdate) entities.Book
+	Delete(book entities.Book) entities.Book
+	IsAllowedToEdit(userID string, bookID uint64) bool
+}
+
+type bookService struct {
+	bookRepository repositories.BookRepository
+}
+
+func NewBookService(bookRepository repositories.BookRepository) BookService {
+	return &bookService{
+		bookRepository: bookRepository,
+	}
+}
+
+func (s *bookService) Index() []entities.Book {
+	return s.bookRepository.Index()
+}
+
+func (s *bookService) Show(bookID uint64) entities.Book {
+	return s.bookRepository.Show(bookID)
+}
+
+func (s *bookService) Store(book dto.BookCreate) entities.Book {
+	data := entities.Book{}
+	err := smapping.FillStruct(&data, smapping.MapFields(&book))
+	if err != nil {
+		log.Fatalf("Failed map %v: ", err)
+	}
+	res := s.bookRepository.Store(data)
+	return res
+}
+
+func (s *bookService) Update(book dto.BookUpdate) entities.Book {
+	data := entities.Book{}
+	err := smapping.FillStruct(&data, smapping.MapFields(&book))
+	if err != nil {
+		log.Fatalf("Failed map %v: ", err)
+	}
+	return s.bookRepository.Update(data)
+}
+
+func (s *bookService) Delete(book entities.Book) entities.Book {
+	return s.bookRepository.Delete(book)
+}
+
+func (s *bookService) IsAllowedToEdit() bool {
+	return true
+}
